273289c3886dd7d60c1397730a235fce71f3df26
Ilya Stubarev <61350234+IlyaStub@users.noreply.github.com>
2025-09-03 08:57:19 +0700

    fix reviewdog 3

--- Task_1_1_1/src/test/java/ru/nsu/g/stubarev/heapsort/SortTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/g/stubarev/heapsort/SortTest.java
@@ -1,9 +1,8 @@
 package ru.nsu.g.stubarev.heapsort;
 
-import org.junit.jupiter.api.Test;
-
-
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.api.Test;
 
 class SortTest {
 
