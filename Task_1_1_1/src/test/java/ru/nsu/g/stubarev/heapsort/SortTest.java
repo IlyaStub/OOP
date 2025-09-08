4de5d3296cd4e2d4247a2e41ac8cdab247b42622
Ilya Stubarev <61350234+IlyaStub@users.noreply.github.com>
2025-09-08 09:09:06 +0700

    я устал уже

--- Task_1_1_1/src/test/java/ru/nsu/g/stubarev/heapsort/SortTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/g/stubarev/heapsort/SortTest.java
@@ -2,8 +2,8 @@ package ru.nsu.g.stubarev.heapsort;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
-import org.junit.jupiter.api.Test;
 import java.util.Random;
+import org.junit.jupiter.api.Test;
 
 class SortTest {
 
@@ -97,9 +97,9 @@ class SortTest {
             long endTime = System.nanoTime();
 
             double timeMs = (endTime - startTime) / 1_000_000.0;
-            double nLogN = n * Math.log(n);
+            double nLogn = n * Math.log(n);
 
-            System.out.printf("%d\t%.2f\t%.2f%n", n, timeMs, nLogN);
+            System.out.printf("%d\t%.2f\t%.2f%n", n, timeMs, nLogn);
 
             for (int j = 1; j < n; j++) {
                 if (array[j - 1] > array[j]) {
