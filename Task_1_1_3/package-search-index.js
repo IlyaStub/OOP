889e138c99f7cd2a4c9807a0e8dd5f910cd3916c
IlyaStub <IlyaStub@users.noreply.github.com>
2025-10-08 14:15:31 +0000

    Deploying to gh-pages from @ IlyaStub/OOP@dd30961af8606afa683a3f11660b6926b35ba31e 🚀

--- Task_1_1_3/package-search-index.js
+++ Task_1_1_3/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.gstubarev.expression"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.gstubarev.expression"},{"l":"ru.nsu.gstubarev.expression.exceptions"}];updateSearchResults();
