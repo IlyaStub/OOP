4f41bd21bedc4b42b604b2f6ece7f57b1706a272
Ilya Stubarev <i.stubarev@g.nsu.ru>
2026-05-21 19:57:46 +0700

    fix test and reviewdogs

--- Task_2_1_2/src/test/java/ru/nsu/gstubarev/topology/worker/WorkerRegistrarTest.java
+++ Task_2_1_2/src/test/java/ru/nsu/gstubarev/topology/worker/WorkerRegistrarTest.java
@@ -1,22 +1,21 @@
 package ru.nsu.gstubarev.topology.worker;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
-/**
- * Test.
- */
 class WorkerRegistrarTest {
 
     @Test
     void testConstructor() {
-        WorkerRegistrar registrar = new WorkerRegistrar("localhost", 9999, 8080);
-        assertThrows(Exception.class, registrar::register);
+        assertDoesNotThrow(() -> {
+            WorkerRegistrar registrar = new WorkerRegistrar("localhost", 9999, 8080);
+        });
     }
 
     @Test
-    void testRegisterWithInvalidMaster() {
+    void testRegisterThrowsWithInvalidMaster() {
         WorkerRegistrar registrar = new WorkerRegistrar("unknown-host", 9999, 8080);
         assertThrows(Exception.class, registrar::register);
     }
